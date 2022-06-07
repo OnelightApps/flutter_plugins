8f685c2475d12d5db20e2da2e56a2f5a135d9e9c
Timophei <snox.jenkins@gmail.com>
2022-06-07 13:48:37 +0300

    Add ringtone mode changed receiver

--- packages/video_player/video_player_android/android/src/main/java/io/flutter/plugins/videoplayer/VideoPlayer.java
+++ packages/video_player/video_player_android/android/src/main/java/io/flutter/plugins/videoplayer/VideoPlayer.java
@@ -7,7 +7,10 @@ package io.flutter.plugins.videoplayer;
 import static com.google.android.exoplayer2.Player.REPEAT_MODE_ALL;
 import static com.google.android.exoplayer2.Player.REPEAT_MODE_OFF;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.media.AudioManager;
 import android.net.Uri;
 import android.view.Surface;
@@ -51,6 +54,8 @@ final class VideoPlayer {
     private static final String FORMAT_HLS = "hls";
     private static final String FORMAT_OTHER = "other";
 
+    private Context context;
+
     private ExoPlayer exoPlayer;
 
     private Surface surface;
@@ -70,6 +75,13 @@ final class VideoPlayer {
 
     private double volume = 1.0;
 
+    private final BroadcastReceiver ringerModeReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            updateVolume();
+        }
+    };
+
     VideoPlayer(
             Context context,
             EventChannel eventChannel,
@@ -78,11 +90,16 @@ final class VideoPlayer {
             String formatHint,
             @NonNull Map<String, String> httpHeaders,
             VideoPlayerOptions options) {
+        this.context = context;
         this.eventChannel = eventChannel;
         this.textureEntry = textureEntry;
         this.options = options;
 
         am = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+
+        IntentFilter filter = new IntentFilter(
+                AudioManager.RINGER_MODE_CHANGED_ACTION);
+        context.registerReceiver(ringerModeReceiver, filter);
 
         ExoPlayer exoPlayer = new ExoPlayer.Builder(context).build();
 
@@ -261,7 +278,7 @@ final class VideoPlayer {
     }
 
     void play() {
-        setVolume(this.volume);
+        updateVolume();
 
         exoPlayer.setPlayWhenReady(true);
     }
@@ -283,6 +300,10 @@ final class VideoPlayer {
         } else {
             exoPlayer.setVolume(bracketedValue);
         }
+    }
+
+    void updateVolume() {
+        setVolume(this.volume);
     }
 
     void setPlaybackSpeed(double value) {
@@ -339,13 +360,18 @@ final class VideoPlayer {
         if (isInitialized) {
             exoPlayer.stop();
         }
+
         textureEntry.release();
         eventChannel.setStreamHandler(null);
+
         if (surface != null) {
             surface.release();
         }
+
         if (exoPlayer != null) {
             exoPlayer.release();
         }
+
+        context.unregisterReceiver(ringerModeReceiver);
     }
 }
